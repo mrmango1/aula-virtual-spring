0db63b0615f9530ba0e7fdb4d05bd8df978dfc57
LyrusDev <aldoguizado11@gmail.com>
2024-02-07 20:02:55 -0500

    first commit

---
+++ src/main/java/com/itsqmet/aulavirtualspring/AulaVirtualSpringApplication.java
@@ -0,0 +1,13 @@
+package com.itsqmet.aulavirtualspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaVirtualSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaVirtualSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsqmet/aulavirtualspring/AulaVirtualSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsqmet.aulavirtualspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaVirtualSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
