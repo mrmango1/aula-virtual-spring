14b66910abd10e2e7e377e21cec508d550eb5ab2
LyrusDev <aldoguizado11@gmail.com>
2024-02-15 22:26:59 -0500

    feat: creación de entidad Curso en la db

---
+++ src/main/java/com/itsqmet/aulavirtualspring/entities/Curso.java
@@ -0,0 +1,31 @@
+package com.itsqmet.aulavirtualspring.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String nombreCurso;
+
+    // Profesor
+    @OneToOne
+    @JoinColumn(name = "profesor_id", referencedColumnName = "id")
+    private Profesor profesor;
+
+    // Estudiantes
+    @ManyToOne
+    @JoinColumn(name = "estudiante_id", referencedColumnName = "id")
+    private Estudiante estudiante;
+
+    // Calificaciones
+//    @OneToMany(mappedBy = "curso")
+//    private List<Calificaciones> calificaciones;
+}
